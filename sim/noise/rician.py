c177e53cfacaecbfa8a7cc7c74fc724ee32527fc
mckib2 <nicholas.bgp@gmail.com>
2018-08-25 11:54:38 -0600

    Added rayleigh distribution with mean and variance functions.

--- sim/noise/rician.py
+++ sim/noise/rician.py
@@ -10,11 +10,13 @@ def rician(M,A,sigma):
     sigma -- standard deviation of the Gaussian noise in the real and the
              imaginary images (which we assume to be equal)
 
+    pM -- computed probability distribution of M
+
     Computes Equation [1] from:
     Gudbjartsson, Hákon, and Samuel Patz. "The Rician distribution of noisy MRI
         data." Magnetic resonance in medicine 34.6 (1995): 910-914.
     '''
-    
+
     sigma2 = sigma*sigma
     pM = M/sigma2*np.exp(-(M*M + A*A)/(2*sigma2))*np.i0(A*M/sigma2)
     return(pM)
