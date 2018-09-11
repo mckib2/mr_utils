5aaccd29355df491a17e475aa4580b88a1ecbe2e
mckib2 <nicholas.bgp@gmail.com>
2018-09-10 23:31:45 -0600

    Update SCGROG test data.

--- mr_utils/load_data/mat.py
+++ mr_utils/load_data/mat.py
@@ -1,37 +1,10 @@
-from scipy.io import loadmat
+import warnings
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore",category=ImportWarning)
+    from scipy.io import loadmat
 
-def load_mat(filename,key):
-    return(loadmat(filename)[key])
-
-# def load_mat(filename,rays=72,raw=False,save=True):
-#     filename_npy = Path(filename).with_suffix('.npy')
-#
-#     # Check to see if we've loaded this before...
-#     if Path(filename_npy).is_file():
-#         print('npy file already created...Loading now...')
-#         t0 = time()
-#         data = np.load(filename_npy)
-#         print('%s loaded in %g sec with dims ' % (filename_npy,time()-t0),end='')
-#         print(data.shape)
-#     else:
-#         print('Loading data...')
-#         t0 = time()
-#         if raw:
-#             key = 'kSpace'
-#         else:
-#             key = 'Image_%d_rays' % rays
-#
-#         data = loadmat(filename)[key]
-#         print('Loaded %s in %g sec with dims ' % (filename,time()-t0),end='')
-#
-#         # Scale to reasonable values
-#         if raw:
-#             scale_data = 10./np.mean(np.abs(data.flatten()))
-#             data *= scale_data
-#
-#         # Save a npy so we can load that in next time
-#         if save:
-#             np.save(filename_npy,data)
-#             print('%s saved!' % filename_npy)
-#
-#     return(data)
+def load_mat(filename,key=None):
+    if key is None:
+        return(loadmat(filename))
+    else:
+        return(loadmat(filename)[key])
