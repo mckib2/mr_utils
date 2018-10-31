bdc942fb9c4db1a2382243febcf8a5bdbbe89bef
mckib2 <nicholas.bgp@gmail.com>
2018-10-31 16:26:34 -0600

    Fixes to view fft logic and need to find better image for multiphase test, as with homogenous T1,T2 without, the only nonzero line is center row of kspace with linear field map...

--- mr_utils/tests/recon/test_multiphase.py
+++ mr_utils/tests/recon/test_multiphase.py
@@ -9,7 +9,7 @@ class MultiphaseTestCase(unittest.TestCase):
     def setUp(self):
         # Load phantom
         dim = 64
-        # phantom = np.rot90(modified_shepp_logan((dim,dim,dim))[:,:,int(dim/2)])
+        phantom = np.rot90(modified_shepp_logan((dim,dim,dim))[:,:,int(dim/2)])
 
         # SSFP params
         self.T1 = 2
@@ -18,21 +18,40 @@ class MultiphaseTestCase(unittest.TestCase):
         self.alpha = np.pi/3
         self.phase_cycs = np.array([ 0,np.pi ])
 
-        # To get periodic banding like we want to see, we need some serious
-        # field inhomogeneity.
-        min_df,max_df = 0,500
-        x = np.linspace(min_df,max_df,dim)
-        y = np.zeros(dim)
-        self.field_map,_ = np.meshgrid(x,y)
+        # # To get periodic banding like we want to see, we need some serious
+        # # field inhomogeneity.
+        # min_df,max_df = 0,500
+        # x = np.linspace(min_df,max_df,dim)
+        # y = np.zeros(dim)
+        # self.field_map,_ = np.meshgrid(x,y)
+        self.field_map = phantom
 
         # Generate simulated banding image explicitly using NMR parameters
-        self.imspace_pc_0 = ssfp(self.T1,self.T2,self.TR,self.alpha,self.field_map,phase_cyc=self.phase_cycs[1])
-        self.kspace_pc_0 = np.fft.fftshift(np.fft.fft2(self.imspace_pc_0))
+        imspace_pc_0 = ssfp(self.T1,self.T2,self.TR,self.alpha,self.field_map,phase_cyc=self.phase_cycs[0])
+        kspace_pc_0 = np.fft.fftshift(np.fft.fft2(imspace_pc_0))
+        imspace_pc_1 = ssfp(self.T1,self.T2,self.TR,self.alpha,self.field_map,phase_cyc=self.phase_cycs[1])
+        kspace_pc_1 = np.fft.fftshift(np.fft.fft2(imspace_pc_1))
+
+        self.imspace = np.stack((imspace_pc_0,imspace_pc_1))
+        self.kspace = np.stack((kspace_pc_0,kspace_pc_1))
 
     def test_multiphase(self):
 
-        view(self.imspace_pc_0)
+        # Check out the images
+        # view(self.imspace)
 
+        # Even lines get pc0, odd lines get pc1
+        kspace_pc0 = self.kspace[0,...]
+        # kspace_pc0[1::2,:] = 0
+        kspace_pc1 = self.kspace[1,...]
+        # kspace_pc1[0::2,:] = 0
+
+        # idc = np.unravel_index(np.argmax(kspace_pc0),kspace_pc0.shape)
+        # print(idc)
+        # kspace_pc1[idc[0],:] = kspace_pc0[idc[0],:]
+
+        view(kspace_pc1,fft=True,fftshift=False)
+        # view(kspace_pc1)
 
 if __name__ == '__main__':
     unittest.main()
