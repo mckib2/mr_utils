d50fcd25482eba6711dc93e24340db130d9fa019
Nicholas <nicholas.bgp@gmail.com>
2018-11-09 00:21:17 -0700

    Skeleton for coil combine methods.

---
+++ mr_utils/coils/coil_combine/coil_pca.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def coil_pca():
+    '''Reduce the dimensionality of the coil dimension using PCA.
+
+    Implements method as described in:
+        Buehrer, Martin, et al. "Array compression for MRI with large coil
+        arrays." Magnetic Resonance in Medicine: An Official Journal of the
+        International Society for Magnetic Resonance in Medicine 57.6 (2007):
+        1131-1139.
+    '''
+    pass
+
+if __name__ == '__main__':
+    pass
