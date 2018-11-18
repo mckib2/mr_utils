746b8ea98da06afcafc377c0ad054353a1f48a8f
mckib2 <nicholas.bgp@gmail.com>
2018-11-17 22:15:03 -0700

    Working on parser, parses everything, but doesn't put everything into tree structure.

--- mr_utils/tests/load_data/test_xprot_parser.py
+++ mr_utils/tests/load_data/test_xprot_parser.py
@@ -1,6 +1,7 @@
 import unittest
 from mr_utils.load_data.xprot_parser import XProtParser
 from mr_utils.test_data import XProtParserTest
+import json
 
 class XProtParserTestCase(unittest.TestCase):
 
@@ -8,10 +9,23 @@ class XProtParserTestCase(unittest.TestCase):
         pass
 
     def test_sample(self):
-        sample = XProtParserTest.sample_xprot()
+        sample = XProtParserTest.full_sample_xprot()
         # print(sample)
         parser = XProtParser()
         parser.parse(sample)
+
+        plongs = parser.structure['XProtocol']['Params']['ParamArray']
+        for p in plongs:
+            if '\"RealDwellTime\"' in p:
+                # if p['parent'] == 'ParamMap."sEFISPEC"':
+                #     ans = [ int(x) for x in p['\"alFree\"'] ]
+                #     break
+                print(p['\"RealDwellTime\"'][0]['Default'])
+        # print(ans)
+
+        # res = parser.structure['XProtocol']['Dependency']
+        # print(json.dumps(res,indent=2))
+
 
 
 if __name__ == '__main__':
