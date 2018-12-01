5e18aeb072df7e4fd55106d32360a5c79a9c219a
mckib2 <nicholas.bgp@gmail.com>
2018-11-30 21:35:11 -0700

    Configs placed into own module, grappa, distributed configs now available out of the box.

---
+++ mr_utils/gadgetron/configs/default.py
@@ -0,0 +1,25 @@
+from mr_utils.gadgetron import GadgetronConfig
+
+def default():
+    config = GadgetronConfig()
+    config.add_reader('1008','GadgetIsmrmrdAcquisitionMessageReader')
+    config.add_reader('1026','GadgetIsmrmrdWaveformMessageReader')
+    config.add_writer('1022','MRIImageWriter')
+    config.add_gadget('RemoveROOversampling')
+    config.add_gadget('AccTrig','AcquisitionAccumulateTriggerGadget',props=[
+        ('trigger_dimension','repetition'),
+        ('sorting_dimension','slice')
+    ])
+    config.add_gadget('Buff','BucketToBufferGadget',props=[
+        ('N_dimension',''),
+        ('S_dimension',''),
+        ('split_slices','true')
+    ])
+    config.add_gadget('SimpleRecon')
+    config.add_gadget('ImageArraySplit')
+    config.add_gadget('Extract')
+    config.add_gadget('ImageFinish')
+    return(config)
+
+if __name__ == '__main__':
+    pass
--- mr_utils/gadgetron/gadgetron_config.py
+++ mr_utils/gadgetron/gadgetron_config.py
@@ -106,51 +106,5 @@ class GadgetronConfig(object):
     #         self.filename = NamedTemporaryFile().name
     #     return(self.filename)
 
-
-
-def default_config():
-    config = GadgetronConfig()
-    config.add_reader('1008','GadgetIsmrmrdAcquisitionMessageReader')
-    config.add_reader('1026','GadgetIsmrmrdWaveformMessageReader')
-    config.add_writer('1022','MRIImageWriter')
-    config.add_gadget('RemoveROOversampling')
-    config.add_gadget('AccTrig','AcquisitionAccumulateTriggerGadget',props=[
-        ('trigger_dimension','repetition'),
-        ('sorting_dimension','slice')
-    ])
-    config.add_gadget('Buff','BucketToBufferGadget',props=[
-        ('N_dimension',''),
-        ('S_dimension',''),
-        ('split_slices','true')
-    ])
-    config.add_gadget('SimpleRecon')
-    config.add_gadget('ImageArraySplit')
-    config.add_gadget('Extract')
-    config.add_gadget('ImageFinish')
-    return(config)
-
-def grappa_cpu_config():
-    config = GadgetronConfig()
-    config.add_reader('1008','GadgetIsmrmrdAcquisitionMessageReader')
-    config.add_reader('1026','GadgetIsmrmrdWaveformMessageReader')
-    config.add_writer('1022','MRIImageWriter')
-    config.add_gadget('NoiseAdjust')
-    config.add_gadget('PCA','PCACoilGadget')
-    config.add_gadget('CoilReduction',props=[
-        ('coils_out','16')
-    ])
-    config.add_gadget('AsymmetricEcho','AsymmetricEchoAdjustROGadget') # RO asymmetric echo handling
-    config.add_gadget('RemoveROOversampling')
-    config.add_gadget('Grappa',props=[
-        ('target_coils','8'),
-        ('use_gpu','false')
-    ])
-    config.add_gadget('GrappaUnmixing')
-    config.add_gadget('Extract')
-    config.add_gadget('AutoScale')
-    config.add_gadget('FloatToShort','FloatToUShortGadget')
-    config.add_gadget('ImageFinish')
-    return(config)
-
 if __name__ == '__main__':
     pass
