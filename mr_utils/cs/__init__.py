1cac09e57561c2989af245e4bccc2eef655fae16
Nicholas <nicholas.bgp@gmail.com>
2019-01-14 10:21:45 -0700

    Make generically encoded IHT_TV function, added docstrings to UFT encoding model class methods.

--- mr_utils/cs/__init__.py
+++ mr_utils/cs/__init__.py
@@ -2,6 +2,7 @@ from .thresholding.iterative_hard_thresholding import *
 from .thresholding.iterative_soft_thresholding import *
 from .thresholding.normalized_iht import *
 from .thresholding.iht_fourier_encoded_total_variation import *
+from .thresholding.iht_tv import *
 from .convex.gd_fourier_encoded_tv import *
 from .convex.gd_tv import *
 from .greedy.cosamp import *
--- mr_utils/cs/models/UFT.py
+++ mr_utils/cs/models/UFT.py
@@ -5,10 +5,23 @@ class UFT(object):
     '''Undersampled Fourier Transform (UFT) data acquisiton model.'''
 
     def __init__(self,samp):
+        '''Initialize with binary sampling pattern.'''
         self.samp = samp
 
     def forward(self,x):
+        '''Fourier encoding with binary undersampling pattern applied.
+
+        This forward transform has no fftshift applied.
+        '''
         return(np.fft.fft2(x)*self.samp)
 
+    def forward_s(self,x):
+        '''Fourier encoding with binary undersampling pattern applied.
+
+        This forward transform applies fftshift before masking.
+        '''
+        return(np.fft.fftshift(np.fft.fft2(x))*self.samp)
+
     def inverse(self,x):
+        '''Inverse fourier encoding.'''
         return(np.fft.ifft2(x))
