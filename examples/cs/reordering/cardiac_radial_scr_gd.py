2bb329c480eeab90d989d93fd155e6b683615a56
mckib2 <nicholas.bgp@gmail.com>
2019-01-22 15:12:07 -0700

    Try updating reordering every iteration.

--- examples/cs/reordering/cardiac_radial_scr_gd.py
+++ examples/cs/reordering/cardiac_radial_scr_gd.py
@@ -3,6 +3,8 @@ from mr_utils.test_data import SCRReordering
 from mr_utils import view
 from mr_utils.cs import GD_TV
 from mr_utils.cs.models import UFT
+from skimage.filters import gaussian
+from skimage.measure import compare_mse,compare_ssim
 
 if __name__ == '__main__':
 
@@ -22,11 +24,47 @@ if __name__ == '__main__':
 
     # Do reconstruction using gradient descent without reordering
     do_reordering = False
-    x_hat_wo = GD_TV(kspace_u,forward_fun=uft.forward,inverse_fun=uft.inverse,alpha=.5,lam=.004,do_reordering=do_reordering,x=imspace,ignore_residual=True,disp=True,maxiter=50)
+    x_hat_wo = GD_TV(kspace_u,forward_fun=uft.forward,inverse_fun=uft.inverse,alpha=.5,lam=.004,do_reordering=do_reordering,x=imspace,ignore_residual=True,disp=False,maxiter=50)
 
     # Do reconstruction using gradient descent with reordering
     do_reordering = True
-    x_hat_w = GD_TV(kspace_u,forward_fun=uft.forward,inverse_fun=uft.inverse,alpha=.5,lam=.004,do_reordering=do_reordering,x=imspace,ignore_residual=True,disp=True,maxiter=50)
+    x_hat_w = GD_TV(kspace_u,forward_fun=uft.forward,inverse_fun=uft.inverse,alpha=.5,lam=.004,do_reordering=do_reordering,x=imspace,ignore_residual=True,disp=False,maxiter=50)
 
-    # Checkout how well we did
-    view(np.hstack((imspace,imspace_u,x_hat_wo,x_hat_w)))
+    # Try iterating on it:
+    x_hat_iter = imspace_u.copy()
+    sigma = 100
+    beta = .8
+    iters = 30
+    lam = .004
+
+    ims = np.zeros(((iters,) + imspace_u.shape),dtype='complex')
+    ssim = np.zeros(iters)
+    err = np.zeros(iters)
+    absimage = np.abs(imspace)
+
+    for ii in range(iters):
+        prior = gaussian(x_hat_iter.real,np.sqrt(sigma)) + 1j*gaussian(x_hat_iter.imag,np.sqrt(sigma))
+        # prior = gaussian(np.abs(x_hat_iter),sigma)*np.exp(-1j*np.angle(x_hat_iter))
+        x_hat_iter = GD_TV(kspace_u,forward_fun=uft.forward,inverse_fun=uft.inverse,alpha=.5,lam=lam,do_reordering=True,x=prior,ignore_residual=True,disp=False,maxiter=50)
+        sigma *= beta
+        # lam /= beta
+
+        err[ii] = compare_mse(absimage,np.abs(x_hat_iter))
+        ssim[ii] = compare_ssim(absimage,np.abs(x_hat_iter))
+        ims[ii,...] = x_hat_iter#*beta
+        print(ii,err[ii],ssim[ii])
+    # recon = np.mean(ims,axis=0)
+    recon = ims[-1,...]
+
+    view(err)
+    view(ssim)
+    # view(np.stack((np.mean(ims[:-1,...],axis=0),x_hat_iter,x_hat_w,x_hat_wo,imspace)))
+
+    print('No reordering  ',np.linalg.norm(imspace/np.linalg.norm(imspace) - x_hat_wo/np.linalg.norm(x_hat_wo)))
+    print('Iterated       ',np.linalg.norm(imspace/np.linalg.norm(imspace) - recon/np.linalg.norm(recon)))
+    print('True reordering',np.linalg.norm(imspace/np.linalg.norm(imspace) - x_hat_w/np.linalg.norm(x_hat_w)))
+
+
+    view(imspace/np.linalg.norm(imspace) - recon/np.linalg.norm(recon))
+    view(imspace/np.linalg.norm(imspace) - x_hat_w/np.linalg.norm(x_hat_w))
+    view(np.stack((recon/np.linalg.norm(recon),x_hat_w/np.linalg.norm(x_hat_w))))
