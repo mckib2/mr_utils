ad85a463c92f09ea38e931b30f8f2f7d5e7bb903
mckib2 <nicholas.bgp@gmail.com>
2019-01-25 04:30:33 -0700

    Clean up CoSaMP code.

--- mr_utils/cs/greedy/cosamp.py
+++ mr_utils/cs/greedy/cosamp.py
@@ -1,10 +1,25 @@
-import numpy as np
-from mr_utils.utils.printtable import Table
+'''Compressive sampling matching pursuit (CoSaMP) algorithm.
+
+This implementation currently does not handle complex signals.
+'''
+
 import logging
 
-logging.basicConfig(format='%(levelname)s: %(message)s',level=logging.DEBUG)
+import numpy as np
 
-def cosamp(A,y,k,lstsq='exact',tol=1e-8,maxiter=500,x=None,disp=False):
+from mr_utils.utils.printtable import Table
+
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
+
+def cosamp(
+        A,
+        y,
+        k,
+        lstsq='exact',
+        tol=1e-8,
+        maxiter=500,
+        x=None,
+        disp=False):
     '''Compressive sampling matching pursuit (CoSaMP) algorithm.
 
     A -- Measurement matrix.
@@ -29,34 +44,43 @@ def cosamp(A,y,k,lstsq='exact',tol=1e-8,maxiter=500,x=None,disp=False):
     '''
 
     # length of measurement vector and original signal
-    n,N = A.shape[:]
+    _n, N = A.shape[:]
 
     # Initializations
-    x_hat = np.zeros(N,dtype=y.dtype)
+    x_hat = np.zeros(N, dtype=y.dtype)
     r = y.copy()
     ynorm = np.linalg.norm(y)
 
     if x is None:
-        x = np.zeros(x_hat.shape,dtype=y.dtype)
+        x = np.zeros(x_hat.shape, dtype=y.dtype)
     elif x.size < x_hat.size:
-        x = np.hstack(([0],x))
+        x = np.hstack(([0], x))
 
     # Decide how we want to solve the intermediate least squares problem
     if lstsq == 'exact':
-        lstsq_fun = lambda A0,y: np.linalg.lstsq(A0,y,rcond=None)[0]
+        lstsq_fun = lambda A0, y: np.linalg.lstsq(A0, y, rcond=None)[0]
     elif lstsq == 'lm':
+        # This also doesn't work very well currently....
         from scipy.optimize import least_squares
-        lstsq_fun = lambda A0,y: least_squares(lambda x: np.linalg.norm(y - np.dot(A0,x)),np.zeros(A0.shape[1],dtype=y.dtype))['x']
+        lstsq_fun = lambda A0, y: least_squares(
+            lambda x: np.linalg.norm(y - np.dot(A0, x)),
+            np.zeros(A0.shape[1], dtype=y.dtype))['x']
     elif lstsq == 'gd':
         # This doesn't work very well...
-        from mr_utils.optimization import gd,fd_complex_step
-        lstsq_fun = lambda A0,y: gd(lambda x: np.linalg.norm(y - np.dot(A0,x)),fd_complex_step,np.zeros(A0.shape[1],dtype=y.dtype),iter=3)[0]
+        from mr_utils.optimization import gd, fd_complex_step
+        lstsq_fun = lambda A0, y: gd(
+            lambda x: np.linalg.norm(y - np.dot(A0, x)),
+            fd_complex_step,
+            np.zeros(A0.shape[1], dtype=y.dtype), iter=3)[0]
     else:
         raise NotImplementedError()
 
     # Start up a table
     if disp:
-        table = Table([ 'iter','norm','MSE' ],[ len(repr(maxiter)),8,8 ],[ 'd','e','e' ])
+        table = Table(
+            ['iter', 'norm', 'MSE'],
+            [len(repr(maxiter)), 8, 8],
+            ['d', 'e', 'e'])
         hdr = table.header()
         for line in hdr.split('\n'):
             logging.info(line)
@@ -64,30 +88,32 @@ def cosamp(A,y,k,lstsq='exact',tol=1e-8,maxiter=500,x=None,disp=False):
     for ii in range(maxiter):
 
         # Get step direction
-        g = np.dot(A.conj().T,r)
+        g = np.dot(A.conj().T, r)
 
         # Add 2*k largest elements of g to support set
-        Tn = np.union1d(x_hat.nonzero()[0],np.argsort(np.abs(g))[-(2*k):])
+        Tn = np.union1d(x_hat.nonzero()[0], np.argsort(np.abs(g))[-(2*k):])
 
         # Solve the least squares problem
-        xn = np.zeros(N,dtype=y.dtype)
-        xn[Tn] = lstsq_fun(A[:,Tn],y)
+        xn = np.zeros(N, dtype=y.dtype)
+        xn[Tn] = lstsq_fun(A[:, Tn], y)
 
         xn[np.argsort(np.abs(xn))[:-k]] = 0
         x_hat = xn.copy()
 
         # Compute new residual
-        r = y - np.dot(A,x_hat)
+        r = y - np.dot(A, x_hat)
 
         # Compute stopping criteria
         stop_criteria = np.linalg.norm(r)/ynorm
 
         # Show MSE at current iteration if we wanted it
         if disp:
-            logging.info(table.row([ ii,stop_criteria,np.mean((np.abs(x - x_hat)**2)) ]))
+            logging.info(
+                table.row(
+                    [ii, stop_criteria, np.mean((np.abs(x - x_hat)**2))]))
 
         # Check stopping criteria
         if stop_criteria < tol:
             break
 
-    return(x_hat)
+    return x_hat
