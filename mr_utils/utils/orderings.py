966511347bb933fad6cf4aa8bac14578b9f55048
mckib2 <nicholas.bgp@gmail.com>
2019-01-25 06:47:32 -0700

    Linted utils.

--- mr_utils/utils/orderings.py
+++ mr_utils/utils/orderings.py
@@ -1,3 +1,9 @@
+'''Methods for orderings for signals.
+
+Methods return flattened indices.
+Hopefully these orderings make the signals more sparse in some domain.
+'''
+
 import numpy as np
 
 def col_stacked_order(x):
@@ -8,7 +14,7 @@ def col_stacked_order(x):
     Note that you might want to provide abs(x) if x is a complex array.
     '''
     idx = np.argsort(x.flatten())
-    return(idx)
+    return idx
 
 def colwise(x):
     '''Find ordering of monotonically varying columns.
@@ -16,10 +22,10 @@ def colwise(x):
     x -- Array to find ordering of.
     '''
     indicies = np.arange(x.size).reshape(x.shape)
-    idx = np.argsort(x,axis=0)
+    idx = np.argsort(x, axis=0)
     for ii in range(x.shape[1]):
-        indicies[:,ii] = indicies[:,ii][idx[:,ii]]
-    return(indicies.flatten())
+        indicies[:, ii] = indicies[:, ii][idx[:, ii]]
+    return indicies.flatten()
 
 def rowwise(x):
     '''Find ordering of monotonically varying rows.
@@ -27,10 +33,10 @@ def rowwise(x):
     x -- Array to find ordering of.
     '''
     indicies = np.arange(x.size).reshape(x.shape)
-    idx = np.argsort(x,axis=1)
+    idx = np.argsort(x, axis=1)
     for ii in range(x.shape[0]):
-        indicies[ii,:] = indicies[ii,:][idx[ii,:]]
-    return(indicies.flatten())
+        indicies[ii, :] = indicies[ii, :][idx[ii, :]]
+    return indicies.flatten()
 
 def inverse_permutation(ordering):
     '''Given some permutation, find the inverse permutation.
@@ -38,17 +44,17 @@ def inverse_permutation(ordering):
     ordering -- Flattened indicies, such as output of np.argsort.
     '''
     inverse_ordering = [0]*len(ordering)
-    for send_from,send_to in enumerate(ordering):
+    for send_from, send_to in enumerate(ordering):
         inverse_ordering[send_to] = send_from
-    return(inverse_ordering)
+    return inverse_ordering
 
 if __name__ == '__main__':
 
     from mr_utils import view
-    data = np.random.random((10,5))
+    data = np.random.random((10, 5))
     # idx = colwise(data)
     idx = rowwise(data)
     view(data.flatten()[idx].reshape(data.shape))
 
     ridx = inverse_permutation(idx)
-    assert np.allclose(data.flatten()[idx][ridx],data.flatten())
+    assert np.allclose(data.flatten()[idx][ridx], data.flatten())
