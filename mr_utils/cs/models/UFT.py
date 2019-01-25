d86a2c2948a8f4cba239ea1c9713c7ffb2938ddb
mckib2 <nicholas.bgp@gmail.com>
2019-01-25 04:35:19 -0700

    Add docs for UFT.

--- mr_utils/cs/models/UFT.py
+++ mr_utils/cs/models/UFT.py
@@ -1,45 +1,55 @@
+'''Undersampled Fourier transform encoding model.
+
+I'm calling "encoding model" how we encode the image domain signal to get to
+the acquisiton domain.  In the case of MR, we measure k-space of the image we
+want, so the encoding model is simply the Fourier transform (ignoring all the
+other complications...).  This object provides methods to go into k-space and
+get back out assuming we undersample according to some mask.
+
+forward_ortho, inverse_ortho are probably the ones you want.
+'''
+
 import numpy as np
-import matplotlib.pyplot as plt
 
 class UFT(object):
     '''Undersampled Fourier Transform (UFT) data acquisiton model.'''
 
-    def __init__(self,samp):
+    def __init__(self, samp):
         '''Initialize with binary sampling pattern.'''
         self.samp = samp
 
-    def forward(self,x):
+    def forward(self, x):
         '''Fourier encoding with binary undersampling pattern applied.
 
         This forward transform has no fftshift applied.
         '''
-        return(np.fft.fft2(x)*self.samp)
+        return np.fft.fft2(x)*self.samp
 
-    def forward_s(self,x):
+    def forward_s(self, x):
         '''Fourier encoding with binary undersampling pattern applied.
 
         This forward transform applies fftshift before masking.
         '''
-        return(np.fft.fftshift(np.fft.fft2(x))*self.samp)
+        return np.fft.fftshift(np.fft.fft2(x))*self.samp
 
-    def forward_ortho(self,x):
+    def forward_ortho(self, x):
         '''Normalized Fourier encoding with binary undersampling.
 
         This forward transform applied fftshift before FFT and after.
         '''
         tmp = np.fft.fftshift(np.fft.fft2(np.fft.fftshift(x)))
         tmp *= self.samp/np.sqrt(tmp.size)
-        return(tmp)
+        return tmp
 
-    def inverse(self,x):
+    def inverse(self, x):
         '''Inverse fourier encoding.'''
-        return(np.fft.ifft2(x))
+        return np.fft.ifft2(x)
 
-    def inverse_ortho(self,x):
+    def inverse_ortho(self, x):
         '''Inverse Normalized Fourier encoding.
 
         This transform applied ifftshift before and after ifft2.
         '''
         tmp = np.fft.ifftshift(np.fft.ifft2(np.fft.ifftshift(x)))
         tmp *= np.sqrt(x.size)
-        return(tmp)
+        return tmp
