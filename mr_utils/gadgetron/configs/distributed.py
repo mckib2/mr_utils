b343b6963b99f9e3eab91a19dc0495568672befe
mckib2 <nicholas.bgp@gmail.com>
2019-01-25 05:30:07 -0700

    Lint all the gadgetron configs.

--- mr_utils/gadgetron/configs/distributed.py
+++ mr_utils/gadgetron/configs/distributed.py
@@ -1,66 +1,86 @@
+'''Example generation of distributed gadget configs.'''
+
 from mr_utils.gadgetron import GadgetronConfig
 
 def distributed_default():
+    '''Generates distributed_default.xml.
+
+    See:
+        https://github.com/gadgetron/gadgetron/blob/master/gadgets/distributed/config/distributed_default.xml
+    '''
     config = GadgetronConfig()
-    config.add_reader('1008','GadgetIsmrmrdAcquisitionMessageReader')
-    config.add_reader('1022','MRIImageReader')
-    config.add_writer('1022','MRIImageWriter')
-    config.add_writer('1008','GadgetIsmrmrdAcquisitionMessageWriter')
-    config.add_gadget('Distribute','IsmrmrdAcquisitionDistributeGadget',dll='gadgetron_distributed',props=[
-        ('parallel_dimension','repetition'),
-        ('use_this_node_for_compute','true')
-    ])
+    config.add_reader('1008', 'GadgetIsmrmrdAcquisitionMessageReader')
+    config.add_reader('1022', 'MRIImageReader')
+    config.add_writer('1022', 'MRIImageWriter')
+    config.add_writer('1008', 'GadgetIsmrmrdAcquisitionMessageWriter')
+    config.add_gadget(
+        'Distribute',
+        'IsmrmrdAcquisitionDistributeGadget',
+        dll='gadgetron_distributed',
+        props=[
+            ('parallel_dimension', 'repetition'),
+            ('use_this_node_for_compute', 'true')
+        ])
     config.add_gadget('RemoveROOversampling')
-    config.add_gadget('AccTrig','AcquisitionAccumulateTriggerGadget',props=[
-        ('trigger_dimension','repetition'),
-        ('sorting_dimension','slice')
+    config.add_gadget('AccTrig', 'AcquisitionAccumulateTriggerGadget', props=[
+        ('trigger_dimension', 'repetition'),
+        ('sorting_dimension', 'slice')
     ])
-    config.add_gadget('Buff','BucketToBufferGadget',props=[
-        ('N_dimension',''),
-        ('S_dimension',''),
-        ('split_slices','true')
+    config.add_gadget('Buff', 'BucketToBufferGadget', props=[
+        ('N_dimension', ''),
+        ('S_dimension', ''),
+        ('split_slices', 'true')
     ])
     config.add_gadget('SimpleRecon')
     config.add_gadget('ImageArraySplit')
-    config.add_gadget('Collect',dll='gadgetron_distributed')
+    config.add_gadget('Collect', dll='gadgetron_distributed')
     config.add_gadget('Extract')
-    config.add_gadget('Sort','ImageSortGadget',props=[
-        ('sorting_dimension','repetition')
+    config.add_gadget('Sort', 'ImageSortGadget', props=[
+        ('sorting_dimension', 'repetition')
     ])
     config.add_gadget('ImageFinish')
-    return(config)
+    return config
 
 def distributed_image_default():
+    '''Generates distributed_image_default.xml.
+
+    See:
+        https://github.com/gadgetron/gadgetron/blob/master/gadgets/distributed/config/distributed_image_default.xml
+    '''
     config = GadgetronConfig()
-    config.add_reader('1008','GadgetIsmrmrdAcquisitionMessageReader')
-    config.add_reader('1022','MRIImageReader')
-    config.add_writer('1022','MRIImageWriter')
-    config.add_writer('1008','GadgetIsmrmrdAcquisitionMessageWriter')
+    config.add_reader('1008', 'GadgetIsmrmrdAcquisitionMessageReader')
+    config.add_reader('1022', 'MRIImageReader')
+    config.add_writer('1022', 'MRIImageWriter')
+    config.add_writer('1008', 'GadgetIsmrmrdAcquisitionMessageWriter')
     config.add_gadget('RemoveROOversampling')
-    config.add_gadget('AccTrig','AcquisitionAccumulateTriggerGadget',props=[
-        ('trigger_dimension','repetition'),
-        ('sorting_dimension','slice')
+    config.add_gadget('AccTrig', 'AcquisitionAccumulateTriggerGadget', props=[
+        ('trigger_dimension', 'repetition'),
+        ('sorting_dimension', 'slice')
     ])
-    config.add_gadget('Buff','BucketToBufferGadget',props=[
-        ('N_dimension',''),
-        ('S_dimension',''),
-        ('split_slices','true')
+    config.add_gadget('Buff', 'BucketToBufferGadget', props=[
+        ('N_dimension', ''),
+        ('S_dimension', ''),
+        ('split_slices', 'true')
     ])
     config.add_gadget('SimpleRecon')
     config.add_gadget('ImageArraySplit')
 
-    config.add_gadget('Distribute','IsmrmrdImageDistributeGadget',dll='gadgetron_distributed',props=[
-        ('parallel_dimension','repetition'),
-        ('use_this_node_for_compute','true'),
-        ('single_package_mode','true')
-    ])
+    config.add_gadget(
+        'Distribute',
+        'IsmrmrdImageDistributeGadget',
+        dll='gadgetron_distributed',
+        props=[
+            ('parallel_dimension', 'repetition'),
+            ('use_this_node_for_compute', 'true'),
+            ('single_package_mode', 'true')
+        ])
     config.add_gadget('Extract')
-    config.add_gadget('Collect',dll='gadgetron_distributed')
-    config.add_gadget('ImageSort',props=[
-        ('sorting_dimension','repetition')
+    config.add_gadget('Collect', dll='gadgetron_distributed')
+    config.add_gadget('ImageSort', props=[
+        ('sorting_dimension', 'repetition')
     ])
     config.add_gadget('ImageFinish')
-    return(config)
+    return config
 
 if __name__ == '__main__':
     pass
--- mr_utils/gadgetron/configs/grappa.py
+++ mr_utils/gadgetron/configs/grappa.py
@@ -1,80 +1,104 @@
+'''Gadgetron configs for GRAPPA gadgets.'''
+
 from mr_utils.gadgetron import GadgetronConfig
 
 def grappa_cpu():
+    '''Generates grappa_cpu.xml.
+
+    See:
+        https://github.com/gadgetron/gadgetron/blob/master/gadgets/grappa/config/grappa_cpu.xml
+    '''
     config = GadgetronConfig()
-    config.add_reader('1008','GadgetIsmrmrdAcquisitionMessageReader')
-    config.add_reader('1026','GadgetIsmrmrdWaveformMessageReader')
-    config.add_writer('1022','MRIImageWriter')
+    config.add_reader('1008', 'GadgetIsmrmrdAcquisitionMessageReader')
+    config.add_reader('1026', 'GadgetIsmrmrdWaveformMessageReader')
+    config.add_writer('1022', 'MRIImageWriter')
     config.add_gadget('NoiseAdjust')
-    config.add_gadget('PCA','PCACoilGadget')
-    config.add_gadget('CoilReduction',props=[
-        ('coils_out','16')
+    config.add_gadget('PCA', 'PCACoilGadget')
+    config.add_gadget('CoilReduction', props=[
+        ('coils_out', '16')
     ])
-    config.add_gadget('AsymmetricEcho','AsymmetricEchoAdjustROGadget') # RO asymmetric echo handling
+    # RO asymmetric echo handling
+    config.add_gadget('AsymmetricEcho', 'AsymmetricEchoAdjustROGadget')
     config.add_gadget('RemoveROOversampling')
-    config.add_gadget('Grappa',props=[
-        ('target_coils','8'),
-        ('use_gpu','false')
+    config.add_gadget('Grappa', props=[
+        ('target_coils', '8'),
+        ('use_gpu', 'false')
     ])
     config.add_gadget('GrappaUnmixing')
     config.add_gadget('Extract')
     config.add_gadget('AutoScale')
-    config.add_gadget('FloatToShort','FloatToUShortGadget')
+    config.add_gadget('FloatToShort', 'FloatToUShortGadget')
     config.add_gadget('ImageFinish')
-    return(config)
+    return config
 
 def grappa_float_cpu():
+    '''Generates grappa_float_cpu.xml.
+
+    See:
+        https://github.com/gadgetron/gadgetron/blob/master/gadgets/grappa/config/grappa_float_cpu.xml
+    '''
     config = GadgetronConfig()
-    config.add_reader('1008','GadgetIsmrmrdAcquisitionMessageReader')
-    config.add_reader('1026','GadgetIsmrmrdWaveformMessageReader')
-    config.add_writer('1022','MRIImageWriter')
+    config.add_reader('1008', 'GadgetIsmrmrdAcquisitionMessageReader')
+    config.add_reader('1026', 'GadgetIsmrmrdWaveformMessageReader')
+    config.add_writer('1022', 'MRIImageWriter')
     config.add_gadget('NoiseAdjust')
-    config.add_gadget('PCA','PCACoilGadget')
-    config.add_gadget('CoilReduction',props=[
-        ('coils_out','16')
+    config.add_gadget('PCA', 'PCACoilGadget')
+    config.add_gadget('CoilReduction', props=[
+        ('coils_out', '16')
     ])
-    config.add_gadget('AsymmetricEcho','AsymmetricEchoAdjustROGadget') # RO asymmetric echo handling
+    # RO asymmetric echo handling
+    config.add_gadget('AsymmetricEcho', 'AsymmetricEchoAdjustROGadget')
     config.add_gadget('RemoveROOversampling')
-    config.add_gadget('Grappa',props=[
-        ('target_coils','8'),
-        ('use_gpu','false')
+    config.add_gadget('Grappa', props=[
+        ('target_coils', '8'),
+        ('use_gpu', 'false')
     ])
     config.add_gadget('GrappaUnmixing')
     config.add_gadget('Extract')
     config.add_gadget('ImageFinish')
-    return(config)
+    return config
 
 def grappa_unoptimized_cpu():
+    '''Generates grappa_unoptimized_cpu.xml.
+
+    See:
+        https://github.com/gadgetron/gadgetron/blob/master/gadgets/grappa/config/grappa_unoptimized.xml
+    '''
     config = GadgetronConfig()
-    config.add_reader('1008','GadgetIsmrmrdAcquisitionMessageReader')
-    config.add_reader('1026','GadgetIsmrmrdWaveformMessageReader')
-    config.add_writer('1022','MRIImageWriter')
+    config.add_reader('1008', 'GadgetIsmrmrdAcquisitionMessageReader')
+    config.add_reader('1026', 'GadgetIsmrmrdWaveformMessageReader')
+    config.add_writer('1022', 'MRIImageWriter')
     config.add_gadget('RemoveROOversampling')
-    config.add_gadget('Grappa',props=[
-        ('target_coils','8'),
-        ('use_gpu','false')
+    config.add_gadget('Grappa', props=[
+        ('target_coils', '8'),
+        ('use_gpu', 'false')
     ])
     config.add_gadget('GrappaUnmixing')
     config.add_gadget('Extract')
     config.add_gadget('AutoScale')
-    config.add_gadget('FloatToShort','FloatToUShortGadget')
+    config.add_gadget('FloatToShort', 'FloatToUShortGadget')
     config.add_gadget('ImageFinish')
-    return(config)
+    return config
 
 def grappa_unoptimized_float_cpu():
+    '''Generates grappa_unoptimized_float_cpu.xml.
+
+    See:
+        https://github.com/gadgetron/gadgetron/blob/master/gadgets/grappa/config/grappa_unoptimized_float.xml
+    '''
     config = GadgetronConfig()
-    config.add_reader('1008','GadgetIsmrmrdAcquisitionMessageReader')
-    config.add_reader('1026','GadgetIsmrmrdWaveformMessageReader')
-    config.add_writer('1022','MRIImageWriter')
+    config.add_reader('1008', 'GadgetIsmrmrdAcquisitionMessageReader')
+    config.add_reader('1026', 'GadgetIsmrmrdWaveformMessageReader')
+    config.add_writer('1022', 'MRIImageWriter')
     config.add_gadget('RemoveROOversampling')
-    config.add_gadget('Grappa',props=[
-        ('target_coils','8'),
-        ('use_gpu','false')
+    config.add_gadget('Grappa', props=[
+        ('target_coils', '8'),
+        ('use_gpu', 'false')
     ])
     config.add_gadget('GrappaUnmixing')
     config.add_gadget('Extract')
     config.add_gadget('ImageFinish')
-    return(config)
+    return config
 
 if __name__ == '__main__':
     pass
