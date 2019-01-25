0deac74089c31ff4724e121aa123c507297f8a90
mckib2 <nicholas.bgp@gmail.com>
2019-01-25 04:18:19 -0700

    GD with generic encoding model and baked in TV cleaned up.

--- mr_utils/cs/convex/gd_tv.py
+++ mr_utils/cs/convex/gd_tv.py
@@ -1,10 +1,24 @@
-import numpy as np
-from mr_utils.utils import dTV
+'''Gradient descent with built in TV and flexible encoding model.'''
+
 import logging
 
-logging.basicConfig(format='%(levelname)s: %(message)s',level=logging.DEBUG)
+import numpy as np
 
-def GD_TV(y,forward_fun,inverse_fun,alpha=.5,lam=.01,do_reordering=False,x=None,ignore_residual=False,disp=False,maxiter=200):
+from mr_utils.utils import dTV
+
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
+
+def GD_TV(
+        y,
+        forward_fun,
+        inverse_fun,
+        alpha=.5,
+        lam=.01,
+        do_reordering=False,
+        x=None,
+        ignore_residual=False,
+        disp=False,
+        maxiter=200):
     '''Gradient descent for a generic encoding model and TV constraint.
 
     y -- Measured data (i.e., y = Ax).
@@ -26,7 +40,7 @@ def GD_TV(y,forward_fun,inverse_fun,alpha=.5,lam=.01,do_reordering=False,x=None,
 
     # Make sure compare_mse is defined
     if x is None:
-        compare_mse = lambda xx,yy: 0
+        compare_mse = lambda xx, yy: 0
         logging.info('No true x provided, MSE will not be calculated.')
     else:
         from skimage.measure import compare_mse
@@ -36,21 +50,24 @@ def GD_TV(y,forward_fun,inverse_fun,alpha=.5,lam=.01,do_reordering=False,x=None,
         if do_reordering:
             from mr_utils.utils.sort2d import sort2d
             from mr_utils.utils.orderings import inverse_permutation
-            _,reordering_r = sort2d(x.real)
-            _,reordering_i = sort2d(x.imag)
+            _, reordering_r = sort2d(x.real)
+            _, reordering_i = sort2d(x.imag)
             inverse_reordering_r = inverse_permutation(reordering_r)
             inverse_reordering_i = inverse_permutation(reordering_i)
 
     # Get some display stuff happening
     if disp:
         from mr_utils.utils.printtable import Table
-        table = Table([ 'iter','norm','MSE' ],[ len(repr(maxiter)),8,8 ],[ 'd','e','e' ])
+        table = Table(
+            ['iter', 'norm', 'MSE'],
+            [len(repr(maxiter)), 8, 8],
+            ['d', 'e', 'e'])
         hdr = table.header()
         for line in hdr.split('\n'):
             logging.info(line)
 
     # Initialize
-    x_hat = np.zeros(y.shape,dtype=y.dtype)
+    x_hat = np.zeros(y.shape, dtype=y.dtype)
     r = -y.copy()
     prev_stop_criteria = np.inf
     norm_y = np.linalg.norm(y)
@@ -65,11 +82,13 @@ def GD_TV(y,forward_fun,inverse_fun,alpha=.5,lam=.01,do_reordering=False,x=None,
         if do_reordering:
             # real part
             xr = x_hat.real.flatten()[reordering_r].reshape(x.shape)
-            second_term_r = dTV(xr).flatten()[inverse_reordering_r].reshape(x.shape)
+            second_term_r = dTV(xr).flatten()[inverse_reordering_r] \
+                .reshape(x.shape)
 
             # imag part
             xi = x_hat.imag.flatten()[reordering_i].reshape(x.shape)
-            second_term_i = dTV(xi).flatten()[inverse_reordering_i].reshape(x.shape)
+            second_term_i = dTV(xi).flatten()[inverse_reordering_i] \
+                .reshape(x.shape)
 
             # put it all together...
             second_term = second_term_r + 1j*second_term_i
@@ -80,7 +99,8 @@ def GD_TV(y,forward_fun,inverse_fun,alpha=.5,lam=.01,do_reordering=False,x=None,
         # Compute stop criteria
         stop_criteria = np.linalg.norm(r)/norm_y
         if not ignore_residual and stop_criteria > prev_stop_criteria:
-            logging.warning('Breaking out of loop after %d iterations. Norm of residual increased!' % ii)
+            logging.warning('Breaking out of loop after %d iterations. \
+                Norm of residual increased!', ii)
             break
         prev_stop_criteria = stop_criteria
 
@@ -89,9 +109,11 @@ def GD_TV(y,forward_fun,inverse_fun,alpha=.5,lam=.01,do_reordering=False,x=None,
 
         # Tell the user what happened
         if disp:
-            logging.info(table.row([ ii,stop_criteria,compare_mse(np.abs(x_hat),xabs) ]))
+            logging.info(
+                table.row(
+                    [ii, stop_criteria, compare_mse(np.abs(x_hat), xabs)]))
 
         # Compute residual
         r = forward_fun(x_hat) - y
 
-    return(x_hat)
+    return x_hat
