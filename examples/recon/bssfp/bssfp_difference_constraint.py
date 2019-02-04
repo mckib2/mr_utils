8d0687cf08e3ff2a97daa95506fbd1fa0da09193
mckib2 <nicholas.bgp@gmail.com>
2019-02-03 18:18:59 -0700

    Needs some more work.  For some reason we can't pin down the location of the second phase cycle..

--- examples/recon/bssfp/bssfp_difference_constraint.py
+++ examples/recon/bssfp/bssfp_difference_constraint.py
@@ -23,23 +23,26 @@ if __name__ == '__main__':
     # Search params
     TR = 6e-3
     dfs = np.linspace(-1/TR, 1/TR, 101)
-    T1s = np.linspace(1, 2, 11)
-    T2s = np.linspace(.5, 2, 11)
+    T1s = np.linspace(1.5, 2, 51)
+    T2s = np.linspace(.5, 1.5, 51)
+    alpha = np.pi
+    keys = get_keys(T1s, T2s, np.atleast_1d(alpha))
 
     # Params for sim
-    alpha = np.pi
-    T1 = 1.8
-    T2 = 0.8
-    df = .3/TR
-    phase_cycs = [0, np.pi]
-    use_offset = True
+    idx_true = np.random.randint(0, keys.shape[1])
+    T1, T2, _ = keys[:, idx_true]
+    df = np.random.choice(dfs)
+    # df = 123.333
+    phase_cycs = [0, np.pi/2]
+    use_diff = False
     add_noise = False
-    sigma = 1e-19 # we need to do some scaling - we're down in the weeds
+    scale_fac = 1e17
+    sigma = 0.25
 
     # Simulate acquisitions
     acqs = np.zeros(len(phase_cycs), dtype='complex')
     for ii, phase_cyc in enumerate(phase_cycs):
-        acqs[ii] = ssfp(T1, T2, TR, alpha, df, phase_cyc=phase_cyc)
+        acqs[ii] = ssfp(T1, T2, TR, alpha, df, phase_cyc=phase_cyc)*scale_fac
 
     # Add complex valued noise Gaussian noise to real/imag channels
     if add_noise:
@@ -47,37 +50,41 @@ if __name__ == '__main__':
             + 1j*np.random.normal(0, sigma, acqs.shape)
 
     # Make a dictionary
-    keys = get_keys(T1s, T2s, np.atleast_1d(alpha))
     D = np.zeros((dfs.size, keys.shape[1]), dtype='complex')
     for ii, df0 in np.ndenumerate(dfs):
-        D[ii, :] = ssfp(keys[0, :], keys[1, :], TR, alpha, field_map=df0)
+        D[ii, :] = ssfp(keys[0, :], keys[1, :], TR, alpha, field_map=df0) \
+            *scale_fac
 
     # Find out where the second acquisition should be relative to first
     idx_away = (phase_cycs[1] - phase_cycs[0])/(2*np.pi)*(1/TR)
     idx_away = np.around(idx_away/(2/TR)*dfs.size).astype(int)
-    # print(idx_away)
+    print(idx_away)
 
     # Find error at each dictionary df
     err = np.zeros(D.shape)
     for ii in range(dfs.size):
-        if use_offset:
-
-            # Use Euclidean distance
-            tmp1 = np.abs(D[ii, :] - acqs[0, None])
-            tmp2 = np.abs(D[ii, :] - acqs[1, None])
-            err[ii, :] = tmp1 + tmp2
-
+        if use_diff:
+            pass
             # # Here's where we need a term to do something with difference
             # diff = acqs[1] - acqs[0]
 
         else:
-            err[ii, :] = np.abs(D[ii, :] - acqs[0, None])
+            # Use Euclidean distance
+            for jj in range(len(phase_cycs)):
+                err[ii, :] += np.abs(D[ii, :] - acqs[jj, None])
+            # err[ii, :] = np.abs(D[ii, :] - acqs[0, None])
 
     # Look up the best match
     idx = np.unravel_index(np.argmin(err), D.shape)
-    plt.plot(dfs, np.abs(D[:, idx[1]]))
-    plt.plot((dfs[idx[0]], dfs[np.mod(idx[0] + idx_away, dfs.size)]),
-             np.abs(acqs), '*')
+    plt.plot(dfs, np.abs(D[:, idx[1]]),
+             (dfs[idx[0]], dfs[np.mod(idx[0] + idx_away, dfs.size)]),
+             np.abs(acqs), '.', label='Found')
+
+    df2 = np.mod(df + (phase_cycs[1] - phase_cycs[0])/(2*np.pi)*(1/TR), 1/TR)
+    plt.plot(dfs, np.abs(D[:, idx_true]), '--', (df, df2), np.abs(acqs), '.',
+             label='True')
+
+    plt.legend()
     plt.show()
 
     # Let's see how we did:
