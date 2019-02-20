d8de5bf29a8ec0a77ea017dcdde24fb85d6b43f0
Nicholas <nicholas.bgp@gmail.com>
2019-02-20 00:05:41 -0700

    Don't plot default zeros or count them as error for off-resonance mapping.

--- examples/recon/bssfp/monte_carlo_planet.py
+++ examples/recon/bssfp/monte_carlo_planet.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     iters = 100
     misses = 0
     asserts = 0
-    df_err = np.zeros(iters)
+    df_err = np.zeros(iters)*np.nan
     for jj in trange(iters, leave=False):
         df_true = np.random.uniform(-1/TR, 1/TR)
         T1_true = np.random.choice(T1s)
@@ -28,7 +28,7 @@ if __name__ == '__main__':
         for ii, pc in enumerate(pcs):
             I[ii] = ssfp(T1_true, T2_true, TR, alpha, df_true, pc)
         try:
-            Meff, T1, T2, df = PLANET(I, alpha, TR, T1s, None, True, True)
+            Meff, T1, T2, df = PLANET(I, alpha, TR, T1s, None, True, False)
             df_err[jj] = np.abs(df_true - df)
             if not np.allclose([T1, T2], [T1_true, T2_true]):
                 misses += 1
@@ -38,6 +38,7 @@ if __name__ == '__main__':
 
     print('%d missed out of %d, %%%g' % (misses, iters, misses/iters*100))
     print('%d assert out of %d, %%%g' % (asserts, iters, asserts/iters*100))
-    print('mean df err: %g hz, std: %g' % (np.mean(df_err), np.std(df_err)))
-    plt.plot(df_err)
+    print('mean df err: %g hz, std: %g' % (
+        np.nanmean(df_err), np.nanstd(df_err)))
+    plt.stem(df_err)
     plt.show()
