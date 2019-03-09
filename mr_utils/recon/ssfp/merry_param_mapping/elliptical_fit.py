Merry's method now up and running for numerical phantoms, seems very sensitive to fieldmap errors.  Still having to add the extra -1 factor to get correct field map estimate, need to compare how MATLAB code runs.

mr_utils/recon/ssfp/merry_param_mapping/elliptical_fit.py
@@ -2,9 +2,9 @@
 
 import numpy as np
 
-from mr_utils.recon.ssfp.merry_param_mapping.ssfp_fit import SSFPfit
+from mr_utils.sim.ssfp import ssfp
 
-def ellipticalfit(Ireal, TE, TR, dphi, offres, M0, alpha, T1, T2):
+def ellipticalfit(Ireal, TR, dphis, offres, M0, alpha, T1, T2):
     '''ELLIPTICALFIT
 
     Ireal - complex pixel hermtian transposed
@@ -20,22 +20,16 @@ def ellipticalfit(Ireal, TE, TR, dphi, offres, M0, alpha, T1, T2):
     T1 *= 100
     T2 *= 10
     offres *= 100 # in Hz
-    num = dphi.size
+    num = dphis.size
 
     Mxans = np.zeros(num)
     Myans = np.zeros(num)
-    for k in range(num):
-        # [Mx0(k,n), My0(k,n), a0(k,n), b0(k,n), M_0(k,n)] = SSFPfit(T1(k,n),
-        # T2(k,n), TR, TE, alpha(k,n), 0, offres(k,n),M0(k,n));
-        Mx, My, _, _, _ = SSFPfit(T1, T2, TR, TE, alpha, dphi[k], offres, M0)
-        I = Mx + 1j*My
+    for ii, dphi in np.ndenumerate(dphis):
+        I = ssfp(
+            T1*1e-3, T2*1e-3, TR*1e-3, alpha, offres, phase_cyc=dphi, M0=M0)
 
         # oposite signs because of the hermitian transpose
-        # print(I.shape, print(Ireal.shape))
-        Mxans[k] = np.real(I) - np.real(Ireal[k])
-        Myans[k] = np.imag(I) + np.imag(Ireal[k])
+        Mxans[ii] = I.real - Ireal[ii].real
+        Myans[ii] = I.imag + Ireal[ii].imag
 
-    # J=(norm([Mxans Myans])); %for fmincon
-    J = np.hstack((Mxans, Myans)) # for lsqnonlin
-
-    return J
+    return np.hstack((Mxans, Myans))

mr_utils/recon/ssfp/merry_param_mapping/plot_ellipse.py
@@ -2,9 +2,9 @@
 
 import numpy as np
 
-from mr_utils.recon.ssfp.merry_param_mapping.ssfp_fit import SSFPfit
+from mr_utils.sim.ssfp import ssfp
 
-def plotEllipse(T1, T2, TR, TE, alpha, offres, M0, dphi):
+def plotEllipse(T1, T2, TR, alpha, offres, M0, dphi):
     '''
     dphi=1 means use fixed linspace for dphi set, else, use the list of dphis
     provided.
@@ -17,18 +17,14 @@ def plotEllipse(T1, T2, TR, TE, alpha, offres, M0, dphi):
         x = np.zeros(dphis.size)
         y = np.zeros(dphis.size)
         for ii, theta in enumerate(dphis):
-            Mx, My, _a, _b, _M = SSFPfit(
-                T1, T2, TR, TE, alpha, theta, offres, M0)
-            x[ii] = Mx
-            y[ii] = My
-
+            Mxy = ssfp(T1, T2, TR, alpha, offres, phase_cyc=theta, M0=M0)
+            x[ii] = Mxy.real
+            y[ii] = Mxy.imag
     else:
         x = np.zeros(dphi.size)
         y = np.zeros(dphi.size)
         for ii in range(dphi.size):
-            Mx, My, _a, _b, _M = SSFPfit(
-                T1, T2, TR, TE, alpha, dphi[ii], offres, M0)
-            x[ii] = Mx
-            y[ii] = My
-
+            Mxy = ssfp(T1, T2, TR, alpha, offres, phase_cyc=dphi[ii], M0=M0)
+            x[ii] = Mxy.real
+            y[ii] = Mxy.imag
     return(x, y)

mr_utils/sim/ssfp/ssfp.py
@@ -91,8 +91,12 @@ def elliptical_params(T1, T2, TR, alpha, M0=1):
         medicine 71.3 (2014): 927-933.
     '''
 
-    E1 = np.exp(-TR/T1)
-    E2 = np.exp(-TR/T2)
+    # All this nonsense so we don't divide by 0
+    E1 = np.zeros(T1.shape)
+    E1[T1 > 0] = np.exp(-TR/T1[T1 > 0])
+    E2 = np.zeros(T2.shape)
+    E2[T2 > 0] = np.exp(-TR/T2[T2 > 0])
+
     ca = np.cos(alpha)
     sa = np.sin(alpha)
     den = 1 - E1*ca - (E2**2)*(E1 - ca)