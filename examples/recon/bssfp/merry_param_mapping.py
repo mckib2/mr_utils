Reverse sign of bssfp phase factor to match that of Taylor implementation, fixes extra negative sign in gs field map calculations.

examples/recon/bssfp/merry_param_mapping.py
@@ -11,7 +11,7 @@ from mr_utils import view
 if __name__ == '__main__':
 
     # First initialize everything
-    N = 64
+    N = 32
     add_noise = False
     disp = True # this will display interesting plots along the way and at end
     num_pcs = 8 # number of phase cycles to generate -- must be divisible by 4
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     T1 = np.zeros((N, N))
     T2 = np.zeros((N, N))
     offres = np.zeros((N, N))
-    alpha = np.ones((N, N))*np.deg2rad(70) # uniform flip angle across image
+    alpha = np.ones((N, N))*np.deg2rad(30) # uniform flip angle across image
     M0 = np.zeros((N, N))
 
     # Make a simple off-resonance map, linear gradient

mr_utils/recon/ssfp/merry_param_mapping/taylor_method.py
@@ -66,46 +66,38 @@ def taylor_method(Is, dphis, alpha, TR, mask=None, chunksize=10, disp=False):
 
         # These plots look fishy -- they changed when I moved from Merry's
         # SSFPFit function to my ssfp simulation function.  Need to look at
-        # MATLAB output to see who's right and/or what's going on.  Probably
-        # related to the fact that ssfp() is adding a bSSFP phase that's 180
-        # degrees wrong, see offresonance map estimation below for discussion.
-        # plt.figure()
-        # plt.title('0 PC')
-        # plt.subplot(2, 2, 1)
-        # plt.plot(Is[0].real[30, 29:228])
-        # plt.subplot(2, 2, 2)
-        # plt.plot(Is[0].imag[30, 29:228])
-        # plt.subplot(2, 2, 3)
-        # plt.plot(np.abs(Is[0][30, 29:228]))
-        # plt.subplot(2, 2, 4)
-        # plt.plot(np.angle(Is[0][30, 29:228]))
-        # plt.show()
-        #
-        # plt.figure()
-        # plt.title('180 PC')
-        # plt.subplot(2, 2, 1)
-        # plt.plot(Is[4].real[30, 29:228])
-        # plt.subplot(2, 2, 2)
-        # plt.plot(Is[4].imag[30, 29:228])
-        # plt.subplot(2, 2, 3)
-        # plt.plot(np.abs(Is[4][30, 29:228]))
-        # plt.subplot(2, 2, 4)
-        # plt.plot(np.angle(Is[4][30, 29:228]))
-        # plt.show()
-        # # compare to Lauzon paper figure 1
+        # MATLAB output to see who's right and/or what's going on.
+        plt.figure()
+        plt.title('0 PC')
+        plt.subplot(2, 2, 1)
+        plt.plot(Is[0].real[30, 29:228])
+        plt.subplot(2, 2, 2)
+        plt.plot(Is[0].imag[30, 29:228])
+        plt.subplot(2, 2, 3)
+        plt.plot(np.abs(Is[0][30, 29:228]))
+        plt.subplot(2, 2, 4)
+        plt.plot(np.angle(Is[0][30, 29:228]))
+        plt.show()
+
+        plt.figure()
+        plt.title('180 PC')
+        plt.subplot(2, 2, 1)
+        plt.plot(Is[4].real[30, 29:228])
+        plt.subplot(2, 2, 2)
+        plt.plot(Is[4].imag[30, 29:228])
+        plt.subplot(2, 2, 3)
+        plt.plot(np.abs(Is[4][30, 29:228]))
+        plt.subplot(2, 2, 4)
+        plt.plot(np.angle(Is[4][30, 29:228]))
+        plt.show()
+        # compare to Lauzon paper figure 1
 
     ## Elliptical fit done here
     offres_est = np.angle(M)
     offres_est = np.unwrap(offres_est, axis=1)
     offres_est[~mask] = 0 #pylint: disable=E1130
     view(offres_est)
-    offres_est /= -np.pi*TR*1e-3
-
-    # TODO:
-    # Notice the negative factor in the above offresonance estimate.  This is
-    # because the bSSFP phase factor being added by ssfp() is 180 degrees off
-    # from where it should be.  Until this gets fixed, we need this negative
-    # sign.
+    offres_est /= np.pi*TR*1e-3
 
     t1map = np.zeros(Is[0].shape)
     t2map = np.zeros(Is[0].shape)

mr_utils/sim/ssfp/ssfp.py
@@ -179,6 +179,9 @@ def get_bssfp_phase(
         bSSFP imaging with an elliptical signal model." Magnetic resonance in
         medicine 71.3 (2014): 927-933.
 
+    We use a positive exponent, exp(i phi), as in Hoff and Taylor MATLAB
+    implementations.
+
     In Hoff's paper the equation is not explicitly given for phi, so we
     implement equation [5] that gives more detailed terms, found in
         Shcherbakova, Yulia, et al. "PLANET: An ellipse fitting approach for
@@ -188,7 +191,7 @@ def get_bssfp_phase(
 
     TE = TR/2 # assume bSSFP
     phi = 2*np.pi*(delta_cs + field_map)*TE + phi_rf + phi_edd + phi_drift
-    phase = np.exp(-1j*phi) # TODO: I think the sign on this wrong
+    phase = np.exp(1j*phi)
     return phase
 
 def get_theta(TR, field_map, phase_cyc=0):