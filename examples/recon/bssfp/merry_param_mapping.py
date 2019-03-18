Added extra phase terms to ssfp simulation, fixed units errors in taylor method (ellipses now line up), and added example showing how phi_rf and off-resonance do similar but different things to ssfp ellipses.

examples/recon/bssfp/merry_param_mapping.py
@@ -10,7 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from mr_utils.recon.ssfp import taylor_method
-from mr_utils.sim.ssfp import ssfp, elliptical_params
+from mr_utils.sim.ssfp import ssfp
 from mr_utils.recon.ssfp.merry_param_mapping.plot_ellipse import plotEllipse
 from mr_utils.utils.ellipse import do_planet_rotation
 from mr_utils import view
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     disp = True # this will display interesting plots along the way and at end
     num_pcs = 16 # number of phase cycles to generate -- must be divisible by 4
     chunksize = 50 # how many pixels to give a cpu core at once
-    TR = 10 # in milliseconds
+    TR = 10e-3 # in seconds
     T1 = np.zeros((N, N))
     T2 = np.zeros((N, N))
     offres = np.zeros((N, N))
@@ -50,16 +50,16 @@ if __name__ == '__main__':
     # 325 T2
     mask0 = np.zeros((N, N)).astype(bool)
     mask0[lo:mid, lo:hi] = True
-    T1[mask0] = 300
-    T2[mask0] = 85
+    T1[mask0] = .300
+    T2[mask0] = .085
     M0[mask0] = 1
 
     # At 3 T water has a T1 of 3000 ms and a T2 of 160 ms. Cartilage 1568 T1 32
     # T2
     mask1 = np.zeros((N, N)).astype(bool)
     mask1[mid+1:hi, lo:hi] = True
-    T1[mask1] = 1200
-    T2[mask1] = 30
+    T1[mask1] = 1.200
+    T2[mask1] = .030
     M0[mask1] = 1
 
     # Get effective mask:
@@ -71,7 +71,7 @@ if __name__ == '__main__':
                                      ' divisible by 4!')
     num_sets = int(num_pcs/4)
     dphis = np.linspace(0, 2*np.pi, num_pcs, endpoint=False)
-    Is = ssfp(T1*1e-3, T2*1e-3, TR*1e-3, alpha, offres, phase_cyc=dphis, M0=M0)
+    Is = ssfp(T1, T2, TR, alpha, offres, phase_cyc=dphis, M0=M0)
 
     ## THIS SECTION IS CURRENTLY BREAKING PYLINT, COMMENTED OUT FOR NOW
     #--------------------------------------------------------------------------
@@ -135,10 +135,10 @@ if __name__ == '__main__':
         row, col = idx[0], idx[1]
         xt, yt = plotEllipse(T1[row, col], T2[row, col], TR,
                              alpha[row, col], offres[row, col], M0[row, col],
-                             1)
+                             True)
         xe, ye = plotEllipse(t1map[row, col], t2map[row, col], TR,
                              alpha[row, col], offresmap[row, col],
-                             m0map[row, col], 1)
+                             m0map[row, col], True)
 
         It = xt + 1j*yt
         Ie = xe + 1j*ye

mr_utils/recon/ssfp/merry_param_mapping/elliptical_fit.py
@@ -1,6 +1,8 @@
 '''Calculates residuals for least_squares fit.'''
 
-import numpy as np
+from ctypes import c_double
+
+# import numpy as np
 
 from mr_utils.sim.ssfp import ssfp
 
@@ -12,7 +14,7 @@ def ellipticalfit(Ireal, TR, dphis, offres, M0, alpha, T1, T2):
     Ireal : array_like
         Hermtian transposed phase-cycle values for single pixel.
     TR : float
-        Repetition time.
+        Repetition time (in sec).
     M0 :
         Estmated proton density from the band reduction algorithm.
     phasecycles :
@@ -26,19 +28,16 @@ def ellipticalfit(Ireal, TR, dphis, offres, M0, alpha, T1, T2):
         Real part of difference concatenated with imaginary part of difference
     '''
 
-    T1 *= 100
-    T2 *= 10
-    offres *= 100 # in Hz
-    num = dphis.size
+    I = ssfp(T1, T2, TR, alpha, offres, phase_cyc=dphis, M0=M0)
+    return (I - Ireal).view(dtype=c_double)
 
-    Mxans = np.zeros(num)
-    Myans = np.zeros(num)
-    for ii, dphi in np.ndenumerate(dphis):
-        I = ssfp(
-            T1*1e-3, T2*1e-3, TR*1e-3, alpha, offres, phase_cyc=dphi, M0=M0)
 
-        # oposite signs because of the hermitian transpose
-        Mxans[ii] = I.real - Ireal[ii].real
-        Myans[ii] = I.imag + Ireal[ii].imag
-
-    return np.hstack((Mxans, Myans))
+    # for ii, dphi in np.ndenumerate(dphis):
+    #     I = ssfp(
+    #         T1, T2, TR, alpha, offres, phase_cyc=dphi, M0=M0)
+    #
+    #     # oposite signs because of the hermitian transpose
+    #     Mxans[ii] = I.real - Ireal[ii].real
+    #     Myans[ii] = I.imag - Ireal[ii].imag
+    #
+    # return np.hstack((Mxans, Myans))

mr_utils/recon/ssfp/merry_param_mapping/optimize.py
@@ -13,31 +13,31 @@ def optimize(I, TR, phasecycles, offres, M0, alpha, T1, T2):
     I : list
         List of phase-cycled images.
     TR : float
-        Repetition time.
+        Repetition time (in sec).
     phasecycles : array_like
         Phase-cycles (in radians).
     offres : array_like
-        Off-resonance map estimation.
+        Off-resonance map estimation (in Hz).
     M0 : float
         Initial guess for M0.
     alpha : float
         Flip angle (in rad).
     T1 : float
-        Inital guess for T1.
+        Inital guess for T1 (in sec).
     T2 : float
-        Initial guess for T2.
+        Initial guess for T2 (in sec).
 
     Returns
     =======
     array_like
-        Optimized values for [T1, T2, offres, M0].
+        Optimized values for [T1 (sec), T2 (sec), offres (Hz), M0].
     float
         Final objective function value.
     '''
 
     # -------- starting point and bounds --------------
-    ub = [30, 50, 1.999, 10] # T1, T2, off resonance 100, alpha in radians, M0
-    lb = [1, 1, 0, 0] # alpha-alpha*.2]; %T1, T2, off resonance 0 Hz, alpha, M0
+    ub = [3, .5, 200, 10] # T1, T2, off resonance Hz, alpha in radians, M0
+    lb = [.1, .01, 0, 0] # T1, T2, off resonance Hz, alpha, M0
     bounds = (lb, ub)
 
     # Make sure offres starts within bounds
@@ -59,5 +59,22 @@ def optimize(I, TR, phasecycles, offres, M0, alpha, T1, T2):
     # -------------------------------------------------
 
     # ------------------ optimize --------------------
+    # import numpy as np
+
+    # from scipy.optimize import brute
+    # robj = lambda x: np.sum(np.abs(obj(x)))
+    # res = brute(robj, list(zip(lb, ub)), Ns=3, disp=True)
+    # return(res, None)
+
+    # from scipy.optimize import shgo
+    # robj = lambda x: np.sum(np.abs(obj(x)))
+    # res = shgo(robj, list(zip(lb, ub)))
+    # return(res['x'], None)
+
+    # from scipy.optimize import dual_annealing
+    # robj = lambda x: np.sum(np.abs(obj(x)))
+    # res = dual_annealing(robj, list(zip(lb, ub)))
+    # return(res['x'], None)
+
     res = least_squares(obj, x0, bounds=bounds)
     return(res['x'], res['cost'])

mr_utils/recon/ssfp/merry_param_mapping/plot_ellipse.py
@@ -10,17 +10,19 @@ def plotEllipse(T1, T2, TR, alpha, offres, M0, dphi):
     Parameters
     ==========
     T1 : float
-        Longitudinal relaxation constant.
+        Longitudinal relaxation constant (in sec).
     T2 : float
-        Transverse relaxation constant.
+        Transverse relaxation constant (in sec).
+    TR : float
+        Repetition time (in sec).
     alpha : float
         Flip angle (in rad).
     offres : float
         Off-resonance (in Hz).
     M0 : float
         Proton density.
-    dphi : float
-        Phase-cycle value (in rad). dphi=1 means use fixed linspace for dphi
+    dphi : float or bool
+        Phase-cycle value (in rad). dphi=True means use fixed linspace for dphi
         set, else, use the list of dphis provided.
 
     Returns
@@ -31,21 +33,12 @@ def plotEllipse(T1, T2, TR, alpha, offres, M0, dphi):
         y coordinate values of ellipse.
     '''
 
-    if dphi == 1:
-        x = []
-        y = []
+    if isinstance(dphi, bool):
         dphis = np.arange(0, 2*np.pi, .01)
-        x = np.zeros(dphis.size)
-        y = np.zeros(dphis.size)
-        for ii, theta in enumerate(dphis):
-            Mxy = ssfp(T1, T2, TR, alpha, offres, phase_cyc=theta, M0=M0)
-            x[ii] = Mxy.real
-            y[ii] = Mxy.imag
+        Mxy = ssfp(T1, T2, TR, alpha, offres, phase_cyc=dphis, M0=M0)
     else:
-        x = np.zeros(dphi.size)
-        y = np.zeros(dphi.size)
-        for ii in range(dphi.size):
-            Mxy = ssfp(T1, T2, TR, alpha, offres, phase_cyc=dphi[ii], M0=M0)
-            x[ii] = Mxy.real
-            y[ii] = Mxy.imag
+        Mxy = ssfp(T1, T2, TR, alpha, offres, phase_cyc=dphis, M0=M0)
+
+    x = Mxy.real
+    y = Mxy.imag
     return(x, y)

mr_utils/recon/ssfp/merry_param_mapping/taylor_method.py
@@ -25,11 +25,11 @@ def optim_wrapper(idx, Is, TR, dphis, offres_est, alpha):
     Is : array_like
         Array of phase-cycled images, (dphi, x, y).
     TR : float
-        Repetition time.
+        Repetition time (in sec).
     dphis : array_like
         Phase-cycles (in radians).
     offres_est : array_like
-        Off-resonance map estimation.
+        Off-resonance map estimation (in Hz).
     alpha : array_like
         Flip angle map (in rad).
 
@@ -45,8 +45,7 @@ def optim_wrapper(idx, Is, TR, dphis, offres_est, alpha):
     ii, jj = idx[0], idx[1]
     I = Is[:, ii, jj]
     xopt, _fopt = optimize(
-        I.conj().T, TR, dphis, offres_est[ii, jj]/100, 1.2, alpha[ii, jj],
-        1000/100, 100/10)
+        I, TR, dphis, offres_est[ii, jj], 1.2, alpha[ii, jj], 1, .1)
     return(ii, jj, xopt)
 
 def taylor_method(Is, dphis, alpha, TR, mask=None, chunksize=10,
@@ -62,7 +61,7 @@ def taylor_method(Is, dphis, alpha, TR, mask=None, chunksize=10,
     alpha : array_like
         Flip angle map (in rad).
     TR : float
-        Repetition time (milliseconds).
+        Repetition time (in sec).
     mask : array_like, optional
         Locations to compute map estimates.
     chunksize : int, optional
@@ -76,11 +75,11 @@ def taylor_method(Is, dphis, alpha, TR, mask=None, chunksize=10,
     Returns
     =======
     t1map : array_like
-        T1 map estimation
+        T1 map estimation (in sec)
     t2map : array_like
-        T2 map estimation
+        T2 map estimation (in sec)
     offresmap : array_like
-        Off-resonance map estimation
+        Off-resonance map estimation (in Hz)
     m0map : array_like
         Proton density map estimation
 
@@ -163,7 +162,7 @@ def taylor_method(Is, dphis, alpha, TR, mask=None, chunksize=10,
     offres_est = np.angle(M)
     m_offres_est = np.ma.array(offres_est, mask=mask & 0)
     offres_est = unwrap_fun(m_offres_est)*mask
-    offres_est /= np.pi*TR*1e-3
+    offres_est /= np.pi*TR
     view(offres_est)
 
     sh = Is.shape[1:]
@@ -185,9 +184,9 @@ def taylor_method(Is, dphis, alpha, TR, mask=None, chunksize=10,
 
     # The answers are then unpacked (not garanteed to be in the right order)
     for ii, jj, xopt in res:
-        t1map[ii, jj] = xopt[0]*100
-        t2map[ii, jj] = xopt[1]*10
-        offresmap[ii, jj] = xopt[2]*100
+        t1map[ii, jj] = xopt[0]
+        t2map[ii, jj] = xopt[1]
+        offresmap[ii, jj] = xopt[2]
         m0map[ii, jj] = xopt[3]
 
     return(t1map, t2map, offresmap, m0map)

mr_utils/sim/ssfp/ssfp.py
@@ -32,7 +32,8 @@ def ssfp_old(T1, T2, TR, alpha, field_map, phase_cyc=0, M0=1):
         Mxy *= get_bssfp_phase(TR, field_map)
     return Mxy
 
-def ssfp(T1, T2, TR, alpha, field_map, phase_cyc=0, M0=1):
+def ssfp(T1, T2, TR, alpha, field_map, phase_cyc=0, M0=1, delta_cs=0, phi_rf=0,
+         phi_edd=0, phi_drift=0):
     r'''SSFP transverse signal at time TE after excitation.
 
     Parameters
@@ -51,6 +52,14 @@ def ssfp(T1, T2, TR, alpha, field_map, phase_cyc=0, M0=1):
         Linear phase-cycle increment (in rad).
     M0 : float or array_like, optional
         proton density.
+    delta_cs : float, optional
+        chemical shift of species w.r.t. the water peak (Hz).
+    phi_rf : float, optional
+        RF phase offset, related to the combin. of Tx/Rx phases (rad).
+    phi_edd : float, optional
+        phase errors due to eddy current effects (rad).
+    phi_drift : float, optional
+        phase errors due to B0 drift (rad).
 
     Returns
     -------
@@ -95,7 +104,7 @@ def ssfp(T1, T2, TR, alpha, field_map, phase_cyc=0, M0=1):
     E2[T2 > 0] = np.exp(-TR/T2[T2 > 0])
 
     # Precompute theta and some cos, sin
-    theta = get_theta(TR, field_map, phase_cyc)
+    theta = get_theta(TR, field_map, phase_cyc, delta_cs)
     ca = np.cos(alpha)
     sa = np.sin(alpha)
     ct = np.cos(theta)
@@ -106,7 +115,7 @@ def ssfp(T1, T2, TR, alpha, field_map, phase_cyc=0, M0=1):
     Mx = M0*((1 - E1)*sa)*(1 - E2*ct)/den
     My = -M0*((1 - E1)*E2*sa*st)/den
     Mxy = Mx + 1j*My
-    Mxy *= get_bssfp_phase(TR, field_map)
+    Mxy *= get_bssfp_phase(TR, field_map, delta_cs, phi_rf, phi_edd, phi_drift)
 
     return Mxy.squeeze()
 
@@ -116,13 +125,13 @@ def elliptical_params(T1, T2, TR, alpha, M0=1):
     Parameters
     ==========
     T1 : array_like
-        longitudinal exponential decay time constant.
+        longitudinal exponential decay time constant (in sec).
     T2 : array_like
-        transverse exponential decay time constant.
+        transverse exponential decay time constant (in sec).
     TR : float
-        repetition time.
+        repetition time (in sec).
     alpha : float
-        flip angle.
+        flip angle (in rad).
     M0 : array_like, optional
         Proton density.
 
@@ -179,7 +188,7 @@ def ssfp_from_ellipse(M, a, b, TR, field_map, phase_cyc=0):
     b : array_like
         Theta-independent ellipse parameter.
     TR : float
-        repetition time.
+        Repetition time (in sec).
     field_map : array_like
         Off-resonance map (in Hz).
     phase_cyc : float
@@ -305,13 +314,13 @@ def get_center_of_mass_nmr(T1, T2, TR, alpha, M0=1):
     Parameters
     ==========
     T1 : array_like
-        longitudinal exponential decay time constant.
+        longitudinal exponential decay time constant (in sec).
     T2 : array_like
-        transverse exponential decay time constant.
+        transverse exponential decay time constant (in sec).
     TR : float
-        repetition time.
+        Repetition time (in sec).
     alpha : float
-        flip angle.
+        Flip angle (in rad).
     M0 : array_like, optional
         Proton density.
 
@@ -334,13 +343,13 @@ def spectrum(T1, T2, TR, alpha):
     Parameters
     ==========
     T1 : array_like
-        longitudinal exponential decay time constant.
+        longitudinal exponential decay time constant (in sec).
     T2 : array_like
-        transverse exponential decay time constant.
+        transverse exponential decay time constant (in sec).
     TR : float
-        repetition time.
+        Repetition time (in sec).
     alpha : float
-        flip angle.
+        Flip angle (in rad).
 
     Returns
     =======
@@ -353,14 +362,14 @@ def spectrum(T1, T2, TR, alpha):
     sig = ssfp_old(T1, T2, TR, alpha, df)
     return sig
 
-def get_bssfp_phase(
-        TR, field_map, delta_cs=0, phi_rf=0, phi_edd=0, phi_drift=0):
+def get_bssfp_phase(TR, field_map, delta_cs=0, phi_rf=0, phi_edd=0,
+                    phi_drift=0):
     '''Additional bSSFP phase factors.
 
     Parameters
     ==========
     TR : float
-        repetition time.
+        repetition time (in sec).
     field_map : array_like
         off-resonance map (Hz).
     delta_cs : float, optional
@@ -404,17 +413,19 @@ def get_bssfp_phase(
     phase = np.exp(1j*phi)
     return phase
 
-def get_theta(TR, field_map, phase_cyc=0):
+def get_theta(TR, field_map, phase_cyc=0, delta_cs=0):
     '''Get theta, spin phase per repetition time, given off-resonance.
 
     Parameters
     ==========
     TR : float
-        repetition time.
+        repetition time (in sec).
     field_map : array_like
         Off-resonance map (in Hz).
-    phase_cyc : array_like
-        Phase-cycling.
+    phase_cyc : array_like, optional
+        Phase-cycling (in rad).
+    delta_cs : float, optional, optional
+        chemical shift of species w.r.t. the water peak (Hz).
 
     Returns
     =======
@@ -423,7 +434,8 @@ def get_theta(TR, field_map, phase_cyc=0):
 
     Notes
     =====
-    Equation for theta=2*pi*df*TR is in Appendix A of [5]_.
+    Equation for theta=2*pi*df*TR is in Appendix A of [5]_.  The additional
+    chemical shift term can be found, e.g., in [4]_.
 
     References
     ==========
@@ -433,7 +445,7 @@ def get_theta(TR, field_map, phase_cyc=0):
            Magnetic Resonance in Medicine 46.1 (2001): 149-158.
     '''
 
-    theta = 2*np.pi*field_map*TR + phase_cyc
+    theta = 2*np.pi*(delta_cs + field_map)*TR + phase_cyc
     return theta
 
 def get_cross_point(I1, I2, I3, I4):