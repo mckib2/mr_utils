41cf055eda776b145b30a903f87f7f1772183b9a
mckib2 <nicholas.bgp@gmail.com>
2019-06-18 12:53:49 -0600

    Add custom config to __init__

--- mr_utils/gadgetron/configs/__init__.py
+++ mr_utils/gadgetron/configs/__init__.py
@@ -4,3 +4,7 @@ from .distributed import *
 from .epi import *
 from .generic import *
 from .python import *
+
+# Custom configurations
+from .generic_cartesian_grappa_coils import (
+    generic_cartesian_grappa_coils)
