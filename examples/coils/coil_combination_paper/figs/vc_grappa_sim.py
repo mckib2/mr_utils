713eff8ddfe01e844e2107978e5b041b2e7c3329
Nicholas <nicholas.bgp@gmail.com>
2019-07-17 20:56:41 -0600

    More messing around with PI and coil combination.  Not looking good...

--- examples/coils/coil_combination_paper/figs/vc_grappa_sim.py
+++ examples/coils/coil_combination_paper/figs/vc_grappa_sim.py
@@ -16,6 +16,7 @@ if __name__ == '__main__':
     ncoils = 32
     mps = birdcage_maps((ncoils, N, N))
     mps = np.moveaxis(mps, 0, -1)
+    use_gcc = False
 
     # Make phase-cycled bSSFP phantom
     ph = shepp_logan(N)
@@ -34,40 +35,93 @@ if __name__ == '__main__':
     imspace = ph[..., None]*mps
     print(imspace.shape)
 
-    # Combine to a number of virtual coils
-    nvcs = 6
-    imspace0 = np.zeros((4, N, N, nvcs), dtype=imspace.dtype)
-    for ii in range(imspace.shape[0]):
-        imspace0[ii, ...] = gcc(
-            imspace[ii, ...], vcoils=nvcs, coil_axis=-1)
-    imspace = imspace0
-
-    from mr_utils import view
-    view(imspace[0, ...])
-
     # Put 'er into  kspace
     ax = (1, 2)
     kspace = 1/np.sqrt(N**2)*np.fft.ifftshift(np.fft.fft2(
         np.fft.fftshift(imspace, axes=ax), axes=ax), axes=ax)
 
+    # crop 20x20 window from the center of k-space for calibration
+    pd = 10
+    ctr = int(N/2)
+    calib = kspace[:, ctr-pd:ctr+pd, ctr-pd:ctr+pd, :].copy()
+
+    # undersample by a factor of 2 in both x and y
+    kspace0 = kspace.copy()
+    kspace0[:, ::2, 1::2, :] = 0
+    kspace0[:, 1::2, ::2, :] = 0
+
+    # Put ACS back in
+    kspace0[:, ctr-pd:ctr+pd, ctr-pd:ctr+pd, :] = calib.copy()
+    mask = np.abs(kspace0) > 0
+
+
+    # Combine to a number of virtual coils
+    nvcs = 5
+
+    if use_gcc:
+        imspace0 = np.zeros((4, N, N, nvcs), dtype=imspace.dtype)
+
+        # Put back in imspace
+        imspace_u = np.sqrt(N**2)*np.fft.ifftshift(np.fft.fft2(
+            np.fft.fftshift(kspace0, axes=ax), axes=ax), axes=ax)
+
+        for ii in range(imspace.shape[0]):
+            imspace0[ii, ...] = gcc(
+                imspace_u[ii, ...], vcoils=nvcs, coil_axis=-1)
+
+        # Put 'er into  kspace
+        ax = (1, 2)
+        kspace = 1/np.sqrt(N**2)*np.fft.ifftshift(np.fft.fft2(
+            np.fft.fftshift(imspace0, axes=ax), axes=ax), axes=ax)
+    else:
+        kspace = np.zeros((4, N, N, nvcs), dtype=imspace.dtype)
+        # Do composite ellipse compbination on groups of coils.
+        # First, we'll cluster coils into nvcs groups:
+        # from scipy.cluster.vq import kmeans2
+        # features = np.reshape(np.abs(imspace_u), (-1, ncoils)).T
+        # _centroids, labels = kmeans2(features, nvcs)
+
+        labels = []
+        step = int(ncoils/nvcs)
+        for ii in range(nvcs):
+            labels += [ii]*step
+        labels = np.array(labels)
+        print(labels)
+
+        # Combine each cluster
+        for jj in range(nvcs):
+            idx = np.argwhere(labels == jj).flatten()
+            print(idx)
+
+            from mr_utils.coils.coil_combine import (
+                simple_composite_ellipse)
+            from mr_utils.utils import sos
+            phase = simple_composite_ellipse(
+                kspace0[..., idx], coil_axis=-1, pc_axis=0)
+            # phase = rigid_composite_ellipse(
+            #     imspace_u[..., idx], coil_axis=-1, pc_axis=0)
+            phase = np.unwrap(phase, axis=0)
+            mag = sos(kspace0[..., idx], axes=-1)
+            kspace[..., jj] = mag*np.exp(1j*phase)
+
+            # from mr_utils import view
+            # view(imspace0[..., jj])
+
+    # from mr_utils import view
+    # view(imspace[0, ...])
+
+    from mr_utils import view
+    view(kspace, log=True)
+
     recon = np.zeros((4, N, N, nvcs), dtype=kspace.dtype)
     for ii in range(imspace.shape[0]):
-        # crop 20x20 window from the center of k-space for calibration
-        pd = 10
-        ctr = int(N/2)
-        calib = kspace[ii, ctr-pd:ctr+pd, ctr-pd:ctr+pd, :].copy()
 
-        # calibrate a kernel
-        kernel_size = (5, 5)
-
-        # undersample by a factor of 2 in both x and y
         kspace0 = kspace[ii, ...].copy()
-        kspace0[::2, 1::2, :] = 0
-        kspace0[1::2, ::2, :] = 0
+        calib = kspace[ii, ctr-pd:ctr+pd, ctr-pd:ctr+pd, :].copy()
 
         # reconstruct:
         res0 = grappa(
-            kspace0, calib, kernel_size, coil_axis=-1,
+            kspace0, calib, (5, 5), coil_axis=-1,
             lamda=0.01, memmap=False)
 
         # Put back into image space
@@ -76,8 +130,8 @@ if __name__ == '__main__':
             np.fft.ifft2(np.fft.ifftshift(
                 res0, axes=ax0), axes=ax0), axes=ax0)
 
-    from mr_utils import view
-    view(recon[0, ...])
+    # from mr_utils import view
+    # view(recon[0, ...])
 
     # Do lGS coil by coil
     lgs = np.zeros((N, N, nvcs), dtype=recon.dtype)
@@ -85,4 +139,6 @@ if __name__ == '__main__':
         lgs[..., cc] = gs_recon(recon[..., cc], pc_axis=0)
 
     from mr_utils import view
-    view(lgs)
+    from mr_utils.utils import sos
+    view(sos(lgs, axes=-1))
+    # view(lgs)
